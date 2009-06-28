e0e53c2f6e591e8696c9a63cee321bd0aad87247
wenchi0920 <wenchi0920@7013c0e8-3aa4-11de-97fb-aff7d2a6cc54>
2009-06-28 08:16:08 +0000

    修改rss及atom通過驗證

--- trunk/f2cont/rss.php
+++ trunk/f2cont/rss.php
@@ -1,8 +1,13 @@
 <?php 
 include_once("include/function.php");
+function htmlspecialchars_decode_php4($uSTR)
+{
+ return strtr($uSTR, array_flip(get_html_translation_table(HTML_ENTITIES, ENT_QUOTES)));
+} 
 
 $saveType=(!empty($_SESSION['rights']) && $_SESSION['rights']=="admin")?"(a.saveType=1 or a.saveType=3)":"a.saveType=1";
 if (!empty($_GET['cateID']) && is_numeric($_GET['cateID'])){
+	$seekcate=$_GET['cateID'];
 	$find_sql="select id from ".$DBPrefix."categories where parent='$seekcate' or id='$seekcate'";
 	$find_result=$DMC->query($find_sql);
 	$str="";
@@ -29,20 +34,21 @@ ob_end_clean();
 header('Content-Type: text/xml; charset=utf-8');
 echo "<?php xml version=\"1.0\" encoding=\"UTF-8\"?> \n";
 ?>
-<rss version="2.0">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
-<title><![CDATA[<?php echo str_replace("&","&amp;",$settingInfo['name']);?>]]></title>
+<title><![CDATA[<?php echo htmlspecialchars_decode_php4($settingInfo['name']);?>]]></title>
 <link><?php echo $home_url?></link>
 <description><![CDATA[<?php echo $settingInfo['blogTitle']?>]]></description>
-<language>utf-8</language>
+<language>zh-tw</language>
 <copyright><![CDATA[<?php echo blogCopyright?>]]></copyright>
-<webMaster><![CDATA[<?php echo $settingInfo['email']?>]]></webMaster>
+<webMaster><![CDATA[<?php echo $settingInfo['email']?> (<?php echo $author?>)]]></webMaster>
+<atom:link href="<?php echo $home_url?>rss.php" rel="self" type="application/rss+xml" />
 <generator><?php echo "F2blog ".blogVersion?></generator> 
 <image>
-	<title><?php echo str_replace("&","&amp;",$settingInfo['name']);?></title> 
+	<title><?php echo htmlspecialchars_decode_php4($settingInfo['name']);?></title> 
 	<url><?php echo $home_url."attachments/".$settingInfo['logo']?></url> 
 	<link><?php echo $home_url?></link> 
-	<description><?php echo str_replace("&","&amp;",$settingInfo['name']);?></description> 
+	<description><?php echo htmlspecialchars_decode_php4($settingInfo['name']);?></description> 
 </image>
 <?php 
 foreach($arr_array as $key=>$fa){
@@ -62,10 +68,10 @@ foreach($arr_array as $key=>$fa){
 ?>
 <item>
 	<link><?php echo $gourl.$fa['cid'].$settingInfo['stype']?></link>
-	<title><![CDATA[<?php echo $fa['logTitle']?><?php echo ($fa['saveType']==3)?" [$strHidLog]":""?>]]></title>
-	<author><?php echo $author?></author>
+	<title><![CDATA[<?php echo htmlspecialchars_decode_php4($fa['logTitle']); ?><?php echo ($fa['saveType']==3)?" [$strHidLog]":""?>]]></title>
+	<author><![CDATA[<?php echo $settingInfo['email']?> (<?php echo $author?>)]]></author>
 	<category><![CDATA[<?php echo $fa['name']?>]]></category>
-	<pubDate><?php echo format_time("L",$fa['postTime'])?></pubDate>
+	<pubDate><?php echo gmdate("D, d M Y H:i:s",$fa['postTime']+8*60*60)." +0800"?></pubDate>
 	<guid><?php echo $gourl.$fa['cid'].$settingInfo['stype']?></guid>	
 	<description><![CDATA[<?php echo $content?>]]></description>
 </item>
