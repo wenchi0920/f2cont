e353a9e1e506eed0cf6d6a943d348d4d62e9ce1c
wenchi0920 <wenchi0920@7013c0e8-3aa4-11de-97fb-aff7d2a6cc54>
2009-07-06 03:38:16 +0000

    F2Blog Hack之SEO篇-分類和標籤頁標題 http://bbs.f2cont.com/thread-201-1-1.html

--- trunk/f2cont/include/loadbar.inc.php
+++ trunk/f2cont/include/loadbar.inc.php
@@ -13,50 +13,71 @@ $logTags="";
 $arr_array=array();
 switch ($load){
 	case "read": //增加本日志阅读量
-		$id=$_GET['id'];
+	$id=$_GET['id'];
 
-		if (empty($_COOKIE['readlog']) || (!empty($_COOKIE['readlog']) && strpos($_COOKIE['readlog'],";$id;")<1)){
-			if (!empty($_COOKIE['readlog'])){
-				$cookie_readlog=$_COOKIE['readlog']."$id;";
-			}else{
-				$cookie_readlog=";;$id;";
-			}	
-			setcookie("readlog", $cookie_readlog, time()+(1*24*3600),$cookiepath,$cookiedomain);
-			$DMC->query("update ".$DBPrefix."logs set viewNums=viewNums+1 WHERE id='$id'");
-		}
-
-		//读取日志
-		$saveType=(!empty($_SESSION['rights']) && $_SESSION['rights']=="admin")?"a.saveType>0":"(a.saveType=1 or a.saveType=2)";
-		$sql="select a.*,b.name,b.cateIcons from ".$DBPrefix."logs as a inner join ".$DBPrefix."categories as b on a.cateId=b.id where $saveType and a.id='$id' order by a.postTime desc";
-		if ($arr_array=$DMC->fetchArray($DMC->query($sql))){
-			$borwseTitle=$arr_array['logTitle']." - ".$arr_array['name'];
-			$logTags=($arr_array['tags']!="")?",".str_replace(";",",",$arr_array['tags']):"";
+	if (empty($_COOKIE['readlog']) || (!empty($_COOKIE['readlog']) && strpos($_COOKIE['readlog'],";$id;")<1)){
+		if (!empty($_COOKIE['readlog'])){
+			$cookie_readlog=$_COOKIE['readlog']."$id;";
 		}else{
-			$borwseTitle="$strErrorNoExistsLog";
+			$cookie_readlog=";;$id;";
 		}
-		$load_file="include/read.inc.php";		
-		break;
+		setcookie("readlog", $cookie_readlog, time()+(1*24*3600),$cookiepath,$cookiedomain);
+		$DMC->query("update ".$DBPrefix."logs set viewNums=viewNums+1 WHERE id='$id'");
+	}
+
+	//读取日志
+	$saveType=(!empty($_SESSION['rights']) && $_SESSION['rights']=="admin")?"a.saveType>0":"(a.saveType=1 or a.saveType=2)";
+	$sql="select a.*,b.name,b.cateIcons from ".$DBPrefix."logs as a inner join ".$DBPrefix."categories as b on a.cateId=b.id where $saveType and a.id='$id' order by a.postTime desc";
+	if ($arr_array=$DMC->fetchArray($DMC->query($sql))){
+		$borwseTitle=$arr_array['logTitle']." - ".$arr_array['name'];
+		$logTags=($arr_array['tags']!="")?",".str_replace(";",",",$arr_array['tags']):"";
+	}else{
+		$borwseTitle="$strErrorNoExistsLog";
+	}
+	$load_file="include/read.inc.php";
+	break;
 	case "applylink": //申请友情连接
-		$load_file="include/applylink.inc.php";
-		$borwseTitle=$strApplyLink;
-		break;
+	$load_file="include/applylink.inc.php";
+	$borwseTitle=$strApplyLink;
+	break;
 	default:
 		if (!empty($arrTopModule[$load])){//模块
 			$borwseTitle=$arrTopModule[$load]['modTitle'];
-			$load_file=$arrTopModule[$load]['pluginPath'];		
+			$load_file=$arrTopModule[$load]['pluginPath'];
 		}else{//默认装载页面
 			if (isset($_GET['disType']) && ($_GET['disType']==1 || $_GET['disType']==0)) {
 				$disType=$_GET['disType'];
-				setcookie("disType", $disType, time()+(365*24*3600),$cookiepath,$cookiedomain);	
+				setcookie("disType", $disType, time()+(365*24*3600),$cookiepath,$cookiedomain);
 			}else if (isset($_COOKIE['disType']) && ($_COOKIE['disType']==1 || $_COOKIE['disType']==0)) {
 				$disType=$_COOKIE['disType'];
 			}else{
 				$disType=$settingInfo['disType'];
-			}			
+			}
 			$load_file="include/content.inc.php";
 			$load="";
 		}
 }
+
+
+//生成分類，標籤標題
+$job=(empty($_REQUEST['job']))?"":$_REQUEST['job'];
+$_POST['seekname']=(empty($_POST['seekname']))?"":$_POST['seekname'];
+$_GET['seekname']=(empty($_GET['seekname']))?"":$_GET['seekname'];
+$seekname=($_POST['seekname']!="")?$_POST['seekname']:$_GET['seekname'];
+switch ($job){
+	case "category":
+		$sql="select name from ".$DBPrefix."categories where id='$seekname'";
+		if ($arr_array=$DMC->fetchArray($DMC->query($sql))){
+			$borwseTitle=$arr_array['name'];
+		}
+		break;
+	case "tags":
+		$borwseTitle=$seekname;
+		break;
+	default:
+		break;
+}
+
 
 //留言本和评论时装载cache.php
 if ($load=="guestbook" || $load=="read") include_once(F2BLOG_ROOT."./include/cache.php");
